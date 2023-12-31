5a098afbbb495d5b4e9662ad5dfb8b1b4f1d8e56
Franco Morroni <fmorroni99@gmail.com>
2023-12-31 04:58:26 -0300

    chore: File rename

---
+++ src/test/java/PomodoroTimerTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import backend.PomodoroTimer;
+
+public class PomodoroTimerTest {
+  // @Test
+  // public void test() throws InterruptedException {
+  //   PomodoroTimer pt = new PomodoroTimer(0);
+  //   pt.setWorkInterval(32).setWorkIntervalsAmount(5).setShortBreakInterval(3.5);
+  //   PomodoroTimer pt2 = new PomodoroTimer(0);
+  //   pt2.setWorkInterval(23);
+  //   double elapsedMins = 10.4;
+  //   assertEquals(pt.getCurrentInterval() - elapsedMins, pt.getRemainingTime(elapsedMins));
+  //   System.out.println(pt);
+  // }
+}
