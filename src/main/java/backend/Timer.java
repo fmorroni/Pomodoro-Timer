93b9209f7f44b8c73b50882a634fc9ccdd651933
Franco Morroni <fmorroni99@gmail.com>
2023-12-31 20:45:39 -0300

    chore: Refactor Timer and make test methods protected

--- src/main/java/backend/Timer.java
+++ src/main/java/backend/Timer.java
@@ -1,59 +1,35 @@
 package backend;
 
 public class Timer {
-  private long start;
-  private long ms;
-  private double seconds, minutes;
+  private Time start;
 
   public Timer() {
-    this(System.currentTimeMillis());
+    this(Time.now());
   }
 
-  public Timer(long start) {
-    if (start < 0) {
-      throw new IllegalArgumentException("start can't be less than 0: start = %d".formatted(start));
+  protected Timer(Time start) {
+    if (start.isNegative()) {
+      throw new IllegalArgumentException("start can't be negative: start = %s".formatted(start));
     }
     this.start = start;
   }
 
   public void restart() {
-    restart(System.currentTimeMillis());
+    restart(Time.now());
   }
 
-  public void restart(long start) {
+  protected void restart(Time start) {
     this.start = start;
-    ms = 0;
-    seconds = 0;
-    minutes = 0;
   }
 
-  public void takeLap() {
-    takeLap(System.currentTimeMillis());
+  public Time getTime() {
+    return getTime(Time.now());
   }
 
-  public void takeLap(long lapMs) {
-    if (lapMs < start) {
-      throw new IllegalArgumentException(
-          "start can't larger than end: %d > %d".formatted(start, lapMs));
+  protected Time getTime(Time endTime) {
+    if (endTime.compareTo(start) < 0) {
+      throw new IllegalArgumentException("End time can't be smaller than start time");
     }
-    ms = lapMs - start;
-    seconds = ms / 1000.0;
-    minutes = seconds / 60;
-  }
-
-  public long getMs() {
-    return ms;
-  }
-
-  public double getSeconds() {
-    return seconds;
-  }
-
-  public double getMinutes() {
-    return minutes;
-  }
-
-  public long getStart() {
-    return start;
+    return endTime.subtract(start);
   }
 }
