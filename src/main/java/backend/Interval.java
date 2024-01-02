b2afc2f4b933a541c76e30e190d6d55da72dbe06
Franco Morroni <fmorroni99@gmail.com>
2024-01-02 00:46:29 -0300

    feat: Add description to Interval enum members

--- src/main/java/backend/Interval.java
+++ src/main/java/backend/Interval.java
@@ -1,7 +1,18 @@
 package backend;
 
 public enum Interval {
-  Work,
-  ShortBreak,
-  LongBreak,
+  Work("Work"),
+  ShortBreak("Short Break"),
+  LongBreak("Long Break");
+
+  private final String description;
+
+  Interval(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return description;
+  }
 }
